b0892db748737655f67d93ada94c19241f078d5c
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-16 10:37:56 -0400

    updated sonar code

--- main.ts
+++ main.ts
@@ -2,6 +2,21 @@
  *
  * Created by: Francisco Rocco Allegri
  * Created on: Oct 2024
- * This program senses sonar in centimetres
+ * This program senses sonar in centimeters
 */
 
+let distanceToObject: number = 0
+
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
